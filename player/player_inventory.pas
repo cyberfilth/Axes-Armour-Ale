22897269a0f1df2c54645033969ff322bd5ad395
CyberFilth <ch22011978@gmail.com>
2020-04-07 21:01:09 +0100

    Inventory screen lists items. Need to fix SIGSEGV

--- player/player_inventory.pas
+++ player/player_inventory.pas
@@ -6,7 +6,7 @@ unit player_inventory;
 interface
 
 uses
-  Graphics, player, items, ui, globalutils;
+  Graphics, SysUtils, player, items, ui, globalutils;
 
 type
   (* Items in inventory *)
@@ -32,16 +32,26 @@ uses
 
 procedure initialiseInventory;
 begin
-  inventory[0].id := 0; inventory[0].Name := 'Empty';
-  inventory[1].id := 1; inventory[1].Name := 'Empty';
-  inventory[2].id := 2; inventory[2].Name := 'Empty';
-  inventory[3].id := 3; inventory[3].Name := 'Empty';
-  inventory[4].id := 4; inventory[4].Name := 'Empty';
-  inventory[5].id := 5; inventory[5].Name := 'Empty';
-  inventory[6].id := 6; inventory[6].Name := 'Empty';
-  inventory[7].id := 7; inventory[7].Name := 'Empty';
-  inventory[8].id := 8; inventory[8].Name := 'Empty';
-  inventory[9].id := 9; inventory[9].Name := 'Empty';
+  inventory[0].id := 0;
+  inventory[0].Name := 'Empty';
+  inventory[1].id := 1;
+  inventory[1].Name := 'Empty';
+  inventory[2].id := 2;
+  inventory[2].Name := 'Empty';
+  inventory[3].id := 3;
+  inventory[3].Name := 'Empty';
+  inventory[4].id := 4;
+  inventory[4].Name := 'Empty';
+  inventory[5].id := 5;
+  inventory[5].Name := 'Empty';
+  inventory[6].id := 6;
+  inventory[6].Name := 'Empty';
+  inventory[7].id := 7;
+  inventory[7].Name := 'Empty';
+  inventory[8].id := 8;
+  inventory[8].Name := 'Empty';
+  inventory[9].id := 9;
+  inventory[9].Name := 'Empty';
 end;
 
 procedure addToInventory(itemNumber: smallint);
@@ -65,16 +75,35 @@ begin
 end;
 
 procedure showInventory;
+var
+  i, x: smallint;
 begin
+  main.gameState := 2; // Accept keyboard commands for inventory screen
+  currentScreen := inventoryScreen; // Display inventory screen
   (* Clear the screen *)
   inventoryScreen.Canvas.Brush.Color := globalutils.BACKGROUNDCOLOUR;
   inventoryScreen.Canvas.FillRect(0, 0, inventoryScreen.Width, inventoryScreen.Height);
   inventoryScreen.Canvas.Font.Color := UITEXTCOLOUR;
-  inventoryScreen.Canvas.TextOut(50, 50, 'Inventory screen');
-  main.gameState := 2; // Accept keyboard commands for inventory screen
-  currentScreen := inventoryScreen; // Display inventory screen
+  inventoryScreen.Canvas.Font.Size := 12;
+  inventoryScreen.Canvas.TextOut(100, 50, 'Inventory screen');
+  inventoryScreen.Canvas.Font.Size := 10;
+  (* List inventory *)
+  x := 60; // x is position of each new line
+  for i := 0 to 9 do
+  begin
+    x := x + 20;
+    if (inventory[i].Name = 'Empty') then
+    begin
+      inventoryScreen.Canvas.Font.Color := MESSAGEFADE1;
+      inventoryScreen.Canvas.TextOut(50, x, IntToStr(i) + ' <empty>');
+    end
+    else
+    begin
+      inventoryScreen.Canvas.Font.Color := UITEXTCOLOUR;
+      inventoryScreen.Canvas.TextOut(50, x, IntToStr(i) + ' ' + inventory[i].Name + ' - ' + itemList[inventory[i].id].itemDescription);
+    end;
+  end;
 end;
 
 
 end.
-
