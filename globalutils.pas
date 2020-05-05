2e7a3425c347e31745d13e56080b77a1f07002fb
CyberFilth <ch22011978@gmail.com>
2020-05-05 11:10:55 +0100

    Disabled range checks on map.pas to fix raised exception class 201 error

--- globalutils.pas
+++ globalutils.pas
@@ -17,7 +17,7 @@ type
 
 const
   (* Version info - a = Alpha, d = Debug, r = Release *)
-  VERSION = '24a';
+  VERSION = '25a';
   (* Save game file *)
   {$IFDEF Linux}
   saveFile = '.axes.data';
