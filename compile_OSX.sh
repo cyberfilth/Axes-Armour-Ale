984f9ad28508baf126d1ba3b7f47a0df64a1dae9
Chris Hawkins <ch22011978@gmail.com>
2022-04-02 09:02:36 +0100

    Updated compile scripts

--- compile_OSX.sh
+++ compile_OSX.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Compiling Axes, Armour & Ale..."
-'/usr/local/bin/fpc' ascii_axe.lpr -Mfpc -Schi -CX -Os3 -Xs -XX -vewnhibq -Filib/x86_64-darwin -Fuscreens -Fudungeons -Fuplayer -Fuvision -Fuitems -Fuitems/weapons -Fuitems/armour -Fuitems/macguffins -Fuentities -Fuentities/fungus -Fuentities/hobs -Fuentities/animals -Fu. -FUlib/x86_64-darwin -FE. -oAxes
+'/usr/local/bin/fpc' Axes.lpr -Mfpc -Schi -CX -Os3 -Xs -XX -vewnhibq -Filib/x86_64-darwin -Fuscreens -Fudungeons -Fuplayer -Fuvision -Fuitems -Fuitems/weapons -Fuitems/armour -Fuitems/macguffins -Fuentities -Fuentities/fungus -Fuentities/hobs -Fuentities/animals -Fu. -FUlib/x86_64-darwin -FE. -oAxes
 echo "Complete."
