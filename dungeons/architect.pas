cae80c43dbab2259f8123b44701ea12e850b28e8
Chris Hawkins <ch22011978@gmail.com>
2022-05-21 18:45:42 +0100

    Started adding location names

--- dungeons/architect.pas
+++ dungeons/architect.pas
@@ -5,6 +5,9 @@
    tDungeon - rooms connected by corridors
    tCrypt - dungeon populated by undead
    tVillage - small settlement
+
+  Arrays containing location types for certain areas of the island.
+  The further north the player goes, the more difficult the locations become
 *)
 
 unit architect;
@@ -14,12 +17,11 @@ unit architect;
 interface
 
 uses
-  SysUtils, universe, island;
+  SysUtils, universe, island, globalUtils;
 
-(* Arrays containing location types for certain areas of the island.
-  The further north the player goes, the more difficult the locations become *)
 var
-  locations1: array[1..3] of dungeonTerrain = (tDungeon, tVillage, tCavern);
+  (* Unique location ID for the locationLookup table *)
+  locationBuilderID: smallint;
 
 (* Bottom row of the island *)
 procedure firstRow;
@@ -29,13 +31,60 @@ procedure seedLocations;
 implementation
 
 procedure firstRow;
+var
+  locations1: array[0..2] of dungeonTerrain = (tDungeon, tVillage, tCavern);
+  locations2: array[0..2] of dungeonTerrain = (tDungeon, tCavern, tDungeon);
+  villageNames: array[0..3] of shortstring =
+    ('village of Barterville', 'village of Flatgrove', 'village of Little Wolding', 'village of Swineford');
+  dungeonNames: array[0..5] of shortstring =
+    ('Abandoned tunnels', 'Abandoned ruins', 'Unknown dungeon', 'Ruins of Cal Arath', 'Whispering ruins', 'Derelict tunnels');
+  cavernNames: array[0..4] of shortstring =
+    ('Pooka caves', 'dark cavern', 'Howling caves', 'deep chasm', 'deep grotto');
+  i, total, choice: byte;
+  (* Used so that no more than one village is created *)
+  villSelected: boolean;
+  placeType: dungeonTerrain;
+  placeName: shortstring;
 begin
-  // the first, bottom row of the island
+   villSelected := False
+   locationBuilderID := 2;
+   total := globalUtils.randomRange(3, 4);
+   i := 0 to total do
+   begin
+     { Choose a location type }
+     choice := Random(2);
+     if (villSelected = False) then
+       placeType := locations1(choice)
+     else
+       placeType := locations2(choice);
+
+     if (placeType = tVillage) then
+       villSelected := True;
+     { Generate a name }
+     if (selection = tVillage) then
+       begin
+         choice := Random(3);
+         placeName := villageNames(choice);
+       end
+     else if (selection = tDungeon) then
+     begin
+         choice := Random(Length(dungeonNames));
+         placeName := dungeonNames(choice);
+         Delete(dungeonNames, choice, 1);
+     end
+     else if (selection = tCavern) then
+     begin
+         choice := Random(Length(cavernNames));
+         placeName := cavernNames(choice);
+         Delete(cavernNames, choice, 1);
+     end;
+     { Place the location }
+   end;
 end;
 
 procedure seedLocations;
 begin
-  // add locations on each row of the island
+  firstRow;
 end;
 
 end.
