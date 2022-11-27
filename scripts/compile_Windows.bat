6bd7a7dba1f039cf0657b82b88949767848c2178
Chris Hawkins <ch22011978@gmail.com>
2022-11-27 19:14:16 +0000

    Updated Windows compiles scripts

--- scripts/compile_Windows.bat
+++ scripts/compile_Windows.bat
@@ -1,1 +1,2 @@
-fpc Axes.lpr -Twin64 -Px86_64 -Mfpc -Schi -CX -Os3 -Xs -XX -vewnhibq -Filib\x86_64-win64 -Fuscreens -Fudungeons -Fuplayer -Fuvision -Fuitems -Fuitems\weapons -Fuitems\armour -Fuitems\macguffins -Fuentities -Fuentities\fungus -Fuentities\hobs -Fuentities\animals -Fuentities\bugs -Fuentities\gnomes -Fu. -FUlib\x86_64-win64 -FE. -oAxes.exe
+cd ..\source
+fpc Axes.pas -MObjFPC -Scghi -CX -Os3 -Xs -XX -l -vewnhibq -Filib\i386-win32 -Fientities -Fidungeons -Fudungeons -Fuentities -Fuitems -Fuplayer -Fuscreens -Fuvision -Fuentities\animals -Fuentities\bugs -Fuentities\fungus -Fuentities\gnomes -Fuentities\hobs -Fuitems\armour -Fuitems\macguffins -Fuitems\weapons -Fuentities\bogles -Fuentities\undead -Fuentities\goblinkin -Fuitems\traps -Fu. -FUlib\i386-win32 -FE. -oAxes.exe
